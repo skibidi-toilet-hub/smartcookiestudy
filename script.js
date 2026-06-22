55291149dcbc7ec4b9bb56be1f770ad27b40bb06
hanna <hanna921135@gmail.com>
2026-06-22 12:31:27 -0400

    change display typo (.toString) and updateDisplay logic
    Signed-off-by: hanna <hanna921135@gmail.com>

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const timer = {
    display: document.getElementById("timerDisplay"),
 };
 
-let studyTime = 25 * 60 
+let studyTime = 1500;
 
-function updateDisplay(time) {
-   hrs = Math.floor(studyTime / 3600);
-   mins = Math.floor((studyTime % 3600) / 60);
-   secs = studyTime % 60; 
-   timer.display.textContent = `${hrs.padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+function updateDisplay() {
+   let hrs = Math.floor(studyTime / 3600);
+   let mins = Math.floor((studyTime % 3600) / 60);
+   let secs = studyTime % 60; 
+   timer.display.textContent = `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
    
 }
 
 timer.startBtn.onclick = function() {
-   if (timer.startBtn.textContent === "Start" || studyTime <= 0) {
+   if (timer.startBtn.textContent === "Start") {
       timer.startBtn.textContent = "Pause";
       countdown = setInterval(() => {
          studyTime--;
-         updateDisplay(studyTime);
+         updateDisplay();
          if (studyTime <= 0) {
             clearInterval(countdown);
             alert("Time's up! Take a break.");
