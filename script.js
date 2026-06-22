ba1dea9fa36f75d3c12188236d71c34d9f026e48
hanna <hanna921135@gmail.com>
2026-06-22 12:05:18 -0400

    basic test of timer logic
    Signed-off-by: hanna <hanna921135@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const timer {
-   const startBtn = document.getElementById("startButton");
-   const resetBtn = document.getElementById("resetButton");
-   const display = document.getElementById("timerDisplay");
-}
+const timer = {
+   startBtn: document.getElementById("startButton"),
+   resetBtn: document.getElementById("resetButton"),
+   display: document.getElementById("timerDisplay"),
+};
 
 let studyTime = 25 * 60 
 
-document.getElement
+function updateDisplay(time) {
+   hrs = Math.floor(studyTime / 3600);
+   mins = Math.floor((studyTime % 3600) / 60);
+   secs = studyTime % 60; 
+   timer.display.textContent = `${hrs.padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+   
+}
+
+timer.startBtn.onclick = function() {
+   if (timer.startBtn.textContent === "Start" || studyTime <= 0) {
+      timer.startBtn.textContent = "Pause";
+      countdown = setInterval(() => {
+         studyTime--;
+         updateDisplay(studyTime);
+         if (studyTime <= 0) {
+            clearInterval(countdown);
+            alert("Time's up! Take a break.");
+         }
+      }
+      , 1000);
+   }
+   else {
+      timer.startBtn.textContent = "Start";
+      clearInterval(countdown);
+   }
+}
+
