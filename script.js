48a88ab64486ac9acccf5434b8270074f32db705
hanna <hanna921135@gmail.com>
2026-06-22 13:47:01 -0400

    correct timer button variables (test 3 or 4)
    Signed-off-by: hanna <hanna921135@gmail.com>

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ let isBreak = false;
 timer.display.textContent = "00:01:00";
 
 
-function updateDisplay(t) {
-   let hrs = Math.floor(t / 3600);
-   let mins = Math.floor((t % 3600) / 60);
-   let secs = t % 60; 
-   timer.display.textContent = `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+function updateDisplay(time) {
+   let hrs = Math.floor(time / 3600);
+   let mins = Math.floor((time % 3600) / 60);
+   let secs = time % 60; 
+   timer.display.textContent = `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
    
 }
 
@@ -27,13 +27,13 @@ countdown = setInterval(() => {
       clearInterval(countdown);
       isBreak = !isBreak;
          }
-      });
+      }, 1000);
    
 }
 
-startBtn.onclick = function() {
-   if (startBtn.textContent === "Start") {
-      startBtn.textContent = "Pause";
+timer.startBtn.onclick = function() {
+   if (timer.startBtn.textContent === "Start") {
+      timer.startBtn.textContent = "Pause";
       if (!isBreak) {
          runTimer(studyTime);
       }
@@ -43,7 +43,7 @@ startBtn.onclick = function() {
 
    }
    else {
-      startBtn.textContent = "Start";
+      timer.startBtn.textContent = "Start";
       clearInterval(countdown);
    }
    
