727e9a16763f4611dc723e06b7771d8cf5ba18f6
hanna <hanna921135@gmail.com>
2026-06-23 12:46:13 -0400

    add settings buttons, test timer logic
    Signed-off-by: hanna <hanna921135@gmail.com>

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const timer = {
    startBtn: document.getElementById("startButton"),
    resetBtn: document.getElementById("resetButton"),
+   settingsBtn0: document.getElementById("closeButton"),
+   settingsBtn1: document.getElementById("openButton"),
    display: document.getElementById("timerDisplay"),
 };
 
-let studyInput = 60;
-let breakInput = 30;
+let studyInput = 600;
+let breakInput = 300;
 let studyTime = studyInput;
 let breakTime = breakInput;
-
 let isBreak = false;
-timer.display.textContent = "00:01:00";
+
+updateDisplay(studyInput);
 
 
 function updateDisplay(time) {
@@ -26,14 +28,13 @@ function runTimer(time, callback) {
 countdown = setInterval(() => {
    time--;
    updateDisplay(time);
-   //changed part:
    if (!isBreak) {
-      studyTime = time;
+      studyTime = time; //sync studyTime with countdown time
    }
    else {
       breakTime = time;
    }
-   //..................
+  
    if (time <= 0) {
       clearInterval(countdown);
       isBreak = !isBreak;
@@ -43,6 +44,21 @@ countdown = setInterval(() => {
    
 }
 
+
+timer.resetBtn.onclick = function() {
+  timer.startBtn.textContent = "Start";
+  if (!isBreak) {
+    updateDisplay(studyInput);
+    studyTime = studyInput;
+  }
+  else {
+    updateDisplay(breakInput);
+    breakTime = breakInput;
+  }
+ 
+}
+
+
 timer.startBtn.onclick = function() {
    if (timer.startBtn.textContent === "Start") {
       timer.startBtn.textContent = "Pause";
@@ -50,7 +66,7 @@ timer.startBtn.onclick = function() {
          studyTime = studyInput; //reset values
          breakTime = breakInput;
          isBreak = true;
-         updateDisplay(breakTime);
+         updateDisplay(breakInput);
          runTimer(breakTime, function() {
             alert("All done!");
             timer.startBtn.textContent = "Start"
@@ -65,20 +81,20 @@ timer.startBtn.onclick = function() {
 }
 
 
+let sidebar = document.getElementById("mySettings");
 
+timer.settingsBtn1.onclick = function() {
+  if (sidebar.style.display === "none") {
+    sidebar.style.display = "block";
+  }
+  else {
+    sidebar.style.display = "none"
+  }
+  
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+timer.settingsBtn0.onclick = function() {
+  if (sidebar.style.display === "block") {
+    sidebar.style.display = "none";
+  }
+}
